f06432e57d8f76f4fc6cdf63451fdcdadb185f41
Daniele Conti <fourlastor@users.noreply.github.com>
2019-11-05 18:57:32 +0000

    publish

---
+++ docs/precache-manifest.c0c26c076246f92ed258ae519f83b54f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2421fc017fd51a9c6a2f3236e72e4559",
+    "url": "/rebrand/index.html"
+  },
+  {
+    "revision": "53b8eb243290a3330e1b",
+    "url": "/rebrand/static/css/main.a9e5327b.chunk.css"
+  },
+  {
+    "revision": "54affef2725067aba3b1",
+    "url": "/rebrand/static/js/2.304090db.chunk.js"
+  },
+  {
+    "revision": "53b8eb243290a3330e1b",
+    "url": "/rebrand/static/js/main.4d14db88.chunk.js"
+  },
+  {
+    "revision": "0c98a9925e272cf32c8e",
+    "url": "/rebrand/static/js/runtime-main.5f9cabbc.js"
+  },
+  {
+    "revision": "07ee9d43137d19c20bace685885fb133",
+    "url": "/rebrand/static/media/audio.07ee9d43.mp3"
+  }
+]);
