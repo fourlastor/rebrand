66b8859a406c6a30261b5e93a311751f9cd59064
Daniele Conti <fourlastor@users.noreply.github.com>
2019-11-05 18:42:44 +0000

    yoooo brand

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import audio from './audio.mp3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      stage: 'input'
+    }
+
+    this.audioRef = React.createRef();
+  }
+
+  play() {
+    const audio = this.audioRef.current
+    setTimeout(() => this.setState({stage: 'newbrand'}), 8144);
+    audio.play()
+    this.setState({ stage: 'oldbrand' })
+  }
+
+  form() {
+    return (
+      <div className="App">
+        <input type="text" className="rebrand-input" placeholder="Insert your name..." value={this.state.name} onChange={(e) => this.updateName(e.target.value)}/>
+        <button type="submit" className="rebrand-input" onClick={() => this.play()}>Rebrand</button>
+      </div>
+    );
+  }
+
+  oldBrand() {
+    return (
+      <div className="App">
+        <div className="oldbrand">{this.state.name}</div>
+      </div>
+    );
+  }
+
+  newBrand() {
+    return (
+      <div className="App">
+        <div className="rebrand">{this.state.name}</div>
+      </div>
+    );
+  }
+
+  updateName(name) {
+    this.setState({name: name})
+  }
+
+  pickDisplay() {
+    switch (this.state.stage) {
+      case 'oldbrand':
+        return this.oldBrand();
+      case 'newbrand':
+        return this.newBrand();
+      case 'input':
+      default:
+        return this.form();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.pickDisplay()}
+        <audio
+          ref={this.audioRef}
+          id="audio"
+          src={audio}
+          preload="auto" />
+      </div>
+    );
+  }
 }
 
 export default App;
